be2a08f6a08e2bd2f73a727323fd414f3850ec81
Long Wei <Bontrey@gmail.com>
2012-10-18 21:36:00 -0400

    registration and login

--- eventd/eventdapp/urls.py
+++ eventd/eventdapp/urls.py
@@ -6,9 +6,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', login, {'template_name': 'eventdapp/login.html'}),
+    url(r'^$', 'eventdapp.views.homepage'),
     url(r'login/$', login, {'template_name': 'eventdapp/login.html'}),
     url(r'logout/$', logout, {'next_page': '..'}),
     url(r'register/$', 'eventdapp.views.register'),
-    url(r'test/$', 'eventdapp.views.test'),
 )
--- eventd/eventdapp/views.py
+++ eventd/eventdapp/views.py
@@ -11,18 +11,26 @@ class CustomUserCreationForm(UserCreationForm):
 
   email = forms.EmailField(required=True)
   gender = forms.ChoiceField(required=False, choices=GENDERS)
+  phone_number = forms.CharField(required=False)
+  location = forms.CharField(required=False)
+  dob = forms.DateField(required=False)
+  photo = forms.ImageField(required=False)
  
   class Meta:
     model = User
-    fields = ("username", "email", "password1", "password2", 'gender')
+    fields = ("username", "email", "password1", "password2", 'gender',
+              'phone_number', 'location', 'dob', 'photo')
  
   def save(self, commit=True):
     user = super(CustomUserCreationForm, self).save(commit=False)
     user.email = self.cleaned_data["email"]
 
     user_profile = UserProfile()
-    if self.cleaned_data["gender"] != '':
-      user_profile.gender = self.cleaned_data["gender"]
+    user_profile.gender = self.cleaned_data["gender"]
+    user_profile.phone_number = self.cleaned_data["phone_number"]
+    user_profile.location = self.cleaned_data["location"]
+    user_profile.dob = self.cleaned_data["dob"]
+    user_profile.photo = self.cleaned_data["photo"]
 
     if commit:
       user.save()
@@ -30,18 +38,18 @@ class CustomUserCreationForm(UserCreationForm):
       user_profile.save()
     return user
 
-def test(request):
-  username = "NoAuth" 
-  if request.user.is_authenticated():
-    username = request.user.get_profile().gender
-  return render_to_response('eventdapp/test.html', {'username':username})
+def homepage(request):
+  if not request.user.is_authenticated():
+    return HttpResponseRedirect("/login/")
+  username = request.user.username
+  return render_to_response('eventdapp/homepage.html', {'username':username})
 
 def register(request):
   if request.method == 'POST':
-    form = CustomUserCreationForm(request.POST)
+    form = CustomUserCreationForm(request.POST, request.FILES)
     if form.is_valid():
       new_user = form.save()
-      return HttpResponseRedirect("/test/")
+      return HttpResponseRedirect("/")
   else:
     form = CustomUserCreationForm()
   return render_to_response("eventdapp/register.html", {
