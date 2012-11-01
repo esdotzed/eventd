463b919ada7940d6a6bb451645b742fbc68f543a
esdotzed <esdotzed@gmail.com>
2012-11-01 12:53:37 -0400

    First attempt to get location info from wifi provider/ GPS.

--- Augmentation/src/com/cs5150/augmentation/MainActivity.java
+++ Augmentation/src/com/cs5150/augmentation/MainActivity.java
@@ -5,6 +5,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -15,13 +18,13 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
-	
-	private static final Context Context = null;
 	private SensorManager mSensorManager;
 	private Sensor mOrientation;
 	private TextView tvDirection;
     private Camera mCamera;
     private CameraPreview mPreview;
+    private LocationManager locationManager;
+
     
 	
 	@Override
@@ -33,47 +36,50 @@ public class MainActivity extends Activity implements SensorEventListener {
 	    tvDirection = (TextView)findViewById(R.id.direction);
         // Create an instance of Camera
         mCamera = getCameraInstance();
-
         // Create our Preview view and set it as the content of our activity.
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+        // Acquire a reference to the system Location Manager
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 	
-	  public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		    // Do something here if sensor accuracy changes.
-		    // You must implement this callback in your code.
-		  }
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	    // Do something here if sensor accuracy changes.
+	    // You must implement this callback in your code.
+	}
 
 		  @Override
-		  protected void onResume() {
-		    super.onResume();
-		    mSensorManager.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_NORMAL);
-		  }
+	protected void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
 		  @Override
-		  protected void onPause() {
-		    super.onPause();
-		    mSensorManager.unregisterListener(this);
-		  }
+	protected void onPause() {
+	    super.onPause();
+	    mSensorManager.unregisterListener(this);
+	}
 
-
-		  public void onSensorChanged(SensorEvent event) {
-		    float azimuth_angle = event.values[0];
-		    float pitch_angle = event.values[1];
-		    float roll_angle = event.values[2];
-		    try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		    // Do something with these orientation angles.
-		    Log.v ("azimuth_angle", "" + azimuth_angle);
-		    Log.v ("pitch_angle","" + pitch_angle);
-		    Log.v ("roll_angle",""+roll_angle);
-		    tvDirection.setText(""+azimuth_angle);
-		  }
+	public void onSensorChanged(SensorEvent event) {
+	    float azimuth_angle = event.values[0];
+	    float pitch_angle = event.values[1];
+	    float roll_angle = event.values[2];
+	    try {
+	    	Thread.sleep(100);
+	    } catch (InterruptedException e) {
+	    	// TODO Auto-generated catch block
+			e.printStackTrace();
+	    }
+		// Do something with these orientation angles.
+		Log.v ("azimuth_angle", "" + azimuth_angle);
+		Log.v ("pitch_angle","" + pitch_angle);
+		Log.v ("roll_angle",""+roll_angle);
+		tvDirection.setText("Direction: "+azimuth_angle);
+	}
 	
 	   @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -93,8 +99,18 @@ public class MainActivity extends Activity implements SensorEventListener {
 	    return c; // returns null if camera is unavailable
 	}
 	   
-	   
-	   
-	   
-	   
+	// Define a listener that responds to location updates
+	private LocationListener locationListener = new LocationListener() {
+	    public void onLocationChanged(Location location) {
+	      // Called when a new location is found by the network location provider.
+	      //makeUseOfNewLocation(location);
+	      Log.v("location",""+location);
+	    }
+
+	    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+	    public void onProviderEnabled(String provider) {}
+
+	    public void onProviderDisabled(String provider) {}
+	  };
 }
