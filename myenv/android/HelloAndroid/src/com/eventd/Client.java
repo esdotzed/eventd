f6320681ff5bb429cc5b7dbd2207eedd2e27741e
esdotzed <esdotzed@gmail.com>
2012-11-28 04:48:07 -0500

    mobile create event, show event time added, augmented reality merged

---
+++ myenv/android/HelloAndroid/src/com/eventd/Client.java
@@ -0,0 +1,14 @@
+package com.eventd;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
+
+public class Client {
+	public static DefaultHttpClient client = new DefaultHttpClient();  
+	
+	public Client()
+	{
+		this.client = client;
+	}
+
+}
---
+++ myenv/android/HelloAndroid/src/com/eventd/Destination.java
@@ -0,0 +1,36 @@
+package com.eventd;
+
+import android.location.Location;
+
+public class Destination {
+	private Location Cur, Des;
+	private float Compass;
+	
+	public Destination(){
+		this.Cur = null;
+		this.Des = null;
+		this.Compass = 0;
+	}
+	
+	public Destination(Location Cur, Location Des, float azimuth_angle){
+		this.Cur = Cur;
+		this.Des = Des;
+		this.Compass = azimuth_angle;
+	}
+	
+	private float getAngle(){
+		double LongOffset = Math.toRadians(Des.getLongitude() - Cur.getLongitude());
+		double LaCur = Math.toRadians(Cur.getLatitude());
+		double LaDes = Math.toRadians(Des.getLatitude());
+		double Y = Math.sin(LongOffset) * Math.cos(LaDes);;
+		double X = Math.cos(LaCur)*Math.sin(LaDes) - Math.sin(LaCur)*Math.cos(LaDes)*Math.cos(LongOffset);
+		double angle = Math.toDegrees(Math.atan2(Y, X));
+	    if(angle < 0) {
+	        angle = 360 - Math.abs(angle);
+	    }
+		return (float)angle - Compass;
+	}
+	public boolean inRange(){
+		return Math.abs(getAngle()) < 10;
+	}
+}
